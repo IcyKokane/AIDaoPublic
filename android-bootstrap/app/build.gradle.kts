39bf2622e8080f507b6679ca2a3a2219d09c35bd
IcyKokane <tomjunker45@gmail.com>
2026-08-14 04:24:14 -0700

    Add bootstrap app module

---
+++ android-bootstrap/app/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins { id("com.android.application") }
+
+android {
+    namespace = "dev.thefoolish.aidao"
+    compileSdk = 37
+
+    defaultConfig {
+        applicationId = "dev.thefoolish.aidao"
+        minSdk = 26
+        targetSdk = 37
+        versionCode = 11
+        versionName = "0.11.0-alpha"
+    }
+}
