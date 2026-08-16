a8334faa591d1f8e722a9274c43c9548af11a3a2
The Foolish One <tomjunker45@gmail.com>
2026-08-16 06:54:35 -0700

    Bump AIDao to 1.0.1 GitHub App auth test build

--- android-bootstrap/app/build.gradle.kts
+++ android-bootstrap/app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "dev.thefoolish.aidao"
         minSdk = 26
         targetSdk = 37
-        versionCode = 100
-        versionName = "1.0.0"
+        versionCode = 101
+        versionName = "1.0.1"
     }
 }
