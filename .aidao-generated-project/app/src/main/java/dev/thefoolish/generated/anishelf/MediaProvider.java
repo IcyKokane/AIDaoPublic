fc2d9cc5a6e435518b1597a2f555750ab28603ce
The Foolish One <tomjunker45@gmail.com>
2026-08-17 02:02:01 -0700

    AIDao generated: app/src/main/java/dev/thefoolish/generated/anishelf/MediaProvider.java

---
+++ .aidao-generated-project/app/src/main/java/dev/thefoolish/generated/anishelf/MediaProvider.java
@@ -0,0 +1,1 @@
+package dev.thefoolish.generated.anishelf;import java.util.List;public interface MediaProvider{String id();String displayName();String health();List<AnimeItem> search(String query)throws Exception;}
