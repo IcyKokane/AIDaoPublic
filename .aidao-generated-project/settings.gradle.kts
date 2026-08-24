8a98da43219ceed6c6895da33ef42b8717316a9a
The Foolish One <tomjunker45@gmail.com>
2026-08-23 22:59:29 -0700

    AIDao generated: settings.gradle.kts

---
+++ .aidao-generated-project/settings.gradle.kts
@@ -0,0 +1,4 @@
+pluginManagement { repositories { google(); mavenCentral(); gradlePluginPortal() } }
+dependencyResolutionManagement { repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS); repositories { google(); mavenCentral() } }
+rootProject.name = "Create A Simple Workout Tracking App. Should"
+include(":app")
