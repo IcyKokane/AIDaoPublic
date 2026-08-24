3f323eb9748d8e73665502fa34a9ecb784394b30
The Foolish One <tomjunker45@gmail.com>
2026-08-23 22:59:34 -0700

    AIDao generated: app/src/main/java/dev/thefoolish/generated/createasimpleworkouttrackingapps/AppNavigator.java

---
+++ .aidao-generated-project/app/src/main/java/dev/thefoolish/generated/createasimpleworkouttrackingapps/AppNavigator.java
@@ -0,0 +1,4 @@
+package dev.thefoolish.generated.createasimpleworkouttrackingapps;
+
+import android.app.Activity;import android.content.Intent;
+public final class AppNavigator { private AppNavigator(){} public static void open(Activity a,Class<? extends Activity> target){a.startActivity(new Intent(a,target));} }
